d8874429871e3510ff424056c282feaecf7b0752
Manuel Reich <mr@wundf.net>
2021-11-23 14:20:40 +0000

    Again

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,5 @@ if [ -n "$INPUT_SKIP_FILE" ]; then
     export $SKIP_FILE = "--skip-file $INPUT_SKIP_FILE"
 fi
 
-OUTPUT = "$INPUT_URL $E $SKIP_FILE"
-sh -c "echo $OUTPUT"
+# OUTPUT = "$INPUT_URL $E $SKIP_FILE"
+sh -c "echo $E"
