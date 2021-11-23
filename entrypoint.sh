2e7dbec9ebd3e314188ab8dc5bed3cc7b303c5ab
Manuel Reich <mr@wundf.net>
2021-11-23 14:26:33 +0000

    Again

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 
 
 if [ -n "$INPUT_CHECKEXTERNALLINKS" ]; then
-    export $E = "-e"
+    export E = "-e"
 fi
 
-if [ -n "$INPUT_SKIP_FILE" ]; then
-    export $SKIP_FILE = "--skip-file $INPUT_SKIP_FILE"
+if [ -n "$INPUT_SKIPFILE" ]; then
+    export SKIP_FILE = "--skip-file $INPUT_SKIP_FILE"
 fi
 
 # OUTPUT = "$INPUT_URL $E $SKIP_FILE"
